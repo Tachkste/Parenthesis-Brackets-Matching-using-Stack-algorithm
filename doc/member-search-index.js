1cadb5359dfab8b5580f706671b9cbdb57562830
ZAWARTO <62563083+ZAWARTO@users.noreply.github.com>
2020-07-09 18:02:38 +0200

    Add files via upload

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"check_brackets","l":"check_brackets()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"check_brackets","l":"main(String[])","url":"main(java.lang.String[])"}]
